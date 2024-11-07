2ba397a1bd7b46d1a872f3f7cb2022d9a846b007
Mina Nabil <minanabil2004@gmail.com>
2024-11-07 02:49:10 +0200

    Create the project structure

---
+++ Backend/src/test/java/com/Paint/Paint/PaintApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Paint.Paint;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaintApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
