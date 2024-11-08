c60adf593a6945d68f925796bf2a122873e5fe89
Mina Nabil <minanabil2004@gmail.com>
2024-11-08 03:23:04 +0200

    Designing the Webpage

--- Frontend/src/Sidebar.jsx
+++ Frontend/src/Sidebar.jsx
@@ -1,6 +1,34 @@
+import { IconButton } from '@mui/material';
+import CircleIcon from '@mui/icons-material/CircleTwoTone';
+import SquareIcon from '@mui/icons-material/SquareTwoTone';
+import TriangleIcon from '@mui/icons-material/ChangeHistoryTwoTone';
+import RectangleIcon from '@mui/icons-material/RectangleTwoTone';
+import LineIcon from '@mui/icons-material/HorizontalRule';
+import ShapesIcon from '@mui/icons-material/CategoryRounded';
+
 function Sidebar(){
     return(
-        <div className="sidebar">Sidebar</div>
+        <div className="sidebar">
+            <br></br>
+            <ShapesIcon sx={{ fontSize: 45 }}/>
+            <p style={{margin: "0", fontFamily: "Arial"}}>Shapes</p>
+            <br></br>
+            <IconButton>
+                <LineIcon color='disabled' fontSize="large"/>
+            </IconButton>
+            <IconButton>
+                <CircleIcon color='disabled' fontSize="large"/>
+            </IconButton>
+            <IconButton>
+                <SquareIcon color='disabled' fontSize="large"/>
+            </IconButton>
+            <IconButton>
+                <RectangleIcon color='disabled' fontSize="large"/>
+            </IconButton>
+            <IconButton>
+                <TriangleIcon color='disabled' fontSize="large"/>
+            </IconButton>
+        </div>
     );
 }
 
--- Frontend/src/Toolbar.jsx
+++ Frontend/src/Toolbar.jsx
@@ -1,6 +1,54 @@
+import { IconButton } from '@mui/material'
+import FillColorIcon from '@mui/icons-material/SquareRounded'
+import BorderColorIcon from '@mui/icons-material/CropDinRounded'
+import BorderWidthIcon from '@mui/icons-material/LineWeightRounded';
+import OpacityIcon from '@mui/icons-material/OpacityTwoTone'
+import UndoIcon from '@mui/icons-material/UndoRounded'
+import RedoIcon from '@mui/icons-material/RedoRounded'
+import SaveIcon from '@mui/icons-material/SaveTwoTone'
+import LoadIcon from '@mui/icons-material/UploadFileTwoTone'
+import CopyIcon from '@mui/icons-material/ContentCopyTwoTone'
+import DeleteIcon from '@mui/icons-material/DeleteTwoTone'
+
 function Toolbar(){
     return(
-        <div className="toolbar">Toolbar</div>
+        <div className="toolbar">
+            <div className='toolbarLeft'>
+                <IconButton>
+                    <FillColorIcon sx={{ fontSize: 30 , color: "#f44336"}}/>
+                </IconButton>
+                <IconButton>
+                    <BorderColorIcon sx={{ fontSize: 30 , color: "#2196f3"}}/>
+                </IconButton>
+                <IconButton>
+                    <BorderWidthIcon color='disabled' sx={{ fontSize: 30 }}/>
+                </IconButton>
+                <IconButton>
+                    <OpacityIcon color='disabled' sx={{ fontSize: 30 }}/>
+                </IconButton>
+                <IconButton>
+                    <UndoIcon color='disabled' sx={{ fontSize: 30 }}/>
+                </IconButton>
+                <IconButton>
+                    <RedoIcon color='disabled' sx={{ fontSize: 30 }}/>
+                </IconButton>
+                <IconButton>
+                    <CopyIcon color='disabled' sx={{ fontSize: 30 }}/>
+                </IconButton>
+                <IconButton>
+                    <DeleteIcon color='disabled' sx={{ fontSize: 30 }}/>
+                </IconButton>
+            </div>
+            <div className='toolbarRight'>
+                <IconButton>
+                    <SaveIcon color='disabled' sx={{ fontSize: 30 }}/>
+                </IconButton>
+                <IconButton>
+                    <LoadIcon color='disabled' sx={{ fontSize: 30 }}/>
+                </IconButton>
+            </div>
+            
+        </div>
     );
 }
 
