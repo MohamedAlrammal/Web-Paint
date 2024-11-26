500bcb5b51afb3c073e9337a2901a9b19837af12
Mina Nabil <minanabil2004@gmail.com>
2024-11-26 21:02:14 +0200

    adding the stage of konva and try drawing some shapes

--- Frontend/src/Paintarea.jsx
+++ Frontend/src/Paintarea.jsx
@@ -1,6 +1,23 @@
+import { Stage, Layer, Rect, Text, Circle, Line } from 'react-konva';
+
 function Paintarea(){
     return(
-        <div className="paintarea">paint here</div>
+        <div id="paintarea">
+            <Stage width={1450} height={545}>
+                <Layer> 
+                <Rect
+          x={200}
+          y={100}
+          width={300}
+          height={100}
+          fill="red"
+          shadowBlur={10}
+        />   
+                <Circle x={1000} y={200} radius={50} fill="green" />
+
+                </Layer>
+            </Stage>
+        </div>
     );
 }
 
