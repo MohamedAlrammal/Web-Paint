a80779d38b68b45e20f881bb75c7f691d2464858
Mina Nabil <minanabil2004@gmail.com>
2024-12-02 07:20:45 +0200

    edit both names

--- Frontend/src/Footer.jsx
+++ Frontend/src/Footer.jsx
@@ -1,7 +1,7 @@
 function Footer(){
     return(
         <footer>
-            &copy; 2024 Developed by <span>Mina Nabil</span>, <span>Abd El-Rahman Khaled</span>, <span>Omar Khalid</span>, <span>Yassin Asaad</span>.
+            &copy; 2024 Developed by <span>Mina Nabil</span>, <span>Abd El-Rhman Khaled</span>, <span>Omar Khaled</span>, <span>Yassin Asaad</span>.
         </footer>
     );
 }
